43a87708a74f3445dcfb1022f710d7c6e60c920d
Ryo Martin <laetuzg@gmail.com>
2023-06-14 13:51:52 +0700

    feat: merged from master

--- app/src/main/java/id/arvigo/arvigomitraapp/di/AppModule.kt
+++ app/src/main/java/id/arvigo/arvigomitraapp/di/AppModule.kt
@@ -5,7 +5,6 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import com.chuckerteam.chucker.api.ChuckerInterceptor
-import id.arvigo.arvigomitraapp.MainActivity
 import id.arvigo.arvigomitraapp.MyApp
 import id.arvigo.arvigomitraapp.data.repository.AddOfferRepository
 import id.arvigo.arvigomitraapp.data.repository.AuthRepository
@@ -17,9 +16,9 @@ import id.arvigo.arvigomitraapp.data.source.network.ApiService
 import id.arvigo.arvigomitraapp.ui.feature.add_offer.AddOfferViewModel
 import id.arvigo.arvigomitraapp.ui.feature.home.HomeViewModel
 import id.arvigo.arvigomitraapp.ui.feature.login.LoginViewModel
-import id.arvigo.arvigomitraapp.ui.feature.register.RegisterDropDownViewModel
 import id.arvigo.arvigomitraapp.ui.feature.product_detail.ProductDetailViewModel
 import id.arvigo.arvigomitraapp.ui.feature.profile.ProfileViewModel
+import id.arvigo.arvigomitraapp.ui.feature.register.RegisterDropDownViewModel
 import id.arvigo.arvigomitraapp.ui.feature.register.RegisterViewModel
 import id.arvigo.arvigomitraapp.ui.feature.splash.SplashViewModel
 import id.arvigo.arvigomitraapp.utils.StaticConstant.BASE_URL
@@ -67,7 +66,6 @@ val viewModelModules = module {
     viewModel { LoginViewModel(get(), get()) }
     viewModel { RegisterDropDownViewModel(get(), get(), get()) }
     viewModel { RegisterViewModel(get()) }
-    viewModel { RegisterViewModel(get(), get(), get()) }
     viewModel { HomeViewModel(get()) }
     viewModel { ProductDetailViewModel(get()) }
     viewModel { AddOfferViewModel(get(), get(), get()) }
